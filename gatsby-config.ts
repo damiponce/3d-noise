847fc079f8bcef24126392e2ef8302eb5556adc4
Damián Ponce <dami.ponce8@gmail.com>
2022-08-13 15:15:14 -0300

    fixed assetPrefix

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -1,15 +1,16 @@
-import type { GatsbyConfig } from "gatsby"
+import type { GatsbyConfig } from 'gatsby';
 
 const config: GatsbyConfig = {
-  siteMetadata: {
-    title: `3D Noise`,
-    siteUrl: `https://www.yourdomain.tld`,
-  },
-  // More easily incorporate content into your pages through automatic TypeScript type generation and better GraphQL IntelliSense.
-  // If you use VSCode you can also use the GraphQL plugin
-  // Learn more at: https://gatsby.dev/graphql-typegen
-  graphqlTypegen: true,
-  plugins: [`gatsby-plugin-sass`],
-}
+   siteMetadata: {
+      title: `3D Noise`,
+      siteUrl: `https://damiponce.github.io/3d-noise/`,
+   },
+   // More easily incorporate content into your pages through automatic TypeScript type generation and better GraphQL IntelliSense.
+   // If you use VSCode you can also use the GraphQL plugin
+   // Learn more at: https://gatsby.dev/graphql-typegen
+   graphqlTypegen: true,
+   plugins: [`gatsby-plugin-sass`],
+   assetPrefix: 'https://damiponce.github.io/3d-noise',
+};
 
-export default config
+export default config;
